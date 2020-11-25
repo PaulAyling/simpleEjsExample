5409cd5034208f8b442b3f470809ef60fa29a47c
Paul AYling <jimmyyukka@gmail.com>
2020-11-25 11:47:36 +0100

    just rendered a static ejs file on its url /ejs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 const app = express();
 
 //make the directory below accessable form the script in html
-app.use('/images', express.static(__dirname + '/images'))
+app.use('/images', express.static(__dirname + '/images'));
 
 //Static page in the root
 app.get('/', function (req, res) {
@@ -12,6 +12,12 @@ app.get('/', function (req, res) {
 //second page in its own url
 app.get('/about', function (req, res) {
 	res.sendFile('about.html', { root: path.join(__dirname, './staticHtml') });
+});
+
+//EJS CODE (it know that templates are in /views)
+app.set('view engine', 'ejs');
+app.get('/ejs', function (req, res) {
+	res.render('index');
 });
 
 app.listen(process.env.PORT || 5000);
