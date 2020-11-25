d22bc5a5e0c5e0f2cf9e9617f96e48dcf2f45bf2
Paul AYling <jimmyyukka@gmail.com>
2020-11-25 11:32:55 +0100

    simple server serving static page & grant access to a folder so that my image can also be displayed

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 var path = require('path');
 const app = express();
 
+//make the directory below accessable form the script in html
+app.use('/images', express.static(__dirname + '/images'))
+
 app.get('/', function (req, res) {
 	res.sendFile('static.html', { root: path.join(__dirname, './staticHtml') });
 });
